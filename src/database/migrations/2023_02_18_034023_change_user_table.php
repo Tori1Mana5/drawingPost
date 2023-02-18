7c17efb620c089531d9ce334bd7a46cf31a7b7b9
鳥井真菜子 <toriimana5@yahoo.co.jp>
2023-02-18 15:22:22 +0900

    内容ちょっと変えてた

--- src/database/migrations/2023_02_18_034023_change_user_table.php
+++ src/database/migrations/2023_02_18_034023_change_user_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->ipAddress('password', 255)->change();
+            $table->string('password', 255)->change();
             $table->renameColumn('nickname', 'nick_name')->change();
-            $table->renameColumn('nick_name', 50)->change();
-            $table->ipAddress('account', 50)->change();
+            $table->string('nickname', 50)->change();
+            $table->string('account', 50)->change();
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate()->change();
         });
     }
@@ -30,10 +30,10 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->ipAddress('password', 45)->change();
+            $table->string('password', 45)->change();
             $table->renameColumn('nick_name', 'nickname')->change();
-            $table->renameColumn('nick_name', 45)->change();
-            $table->ipAddress('account', 45)->change();
+            $table->string('nick_name', 45)->change();
+            $table->string('account', 45)->change();
             $table->dateTime('updated_at')->useCurrentOnUpdate()->change();
         });
     }
