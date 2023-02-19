7aa08dac6f8dbb3a2e288d059a66d56de350dcdc
鳥井真菜子 <toriimana5@yahoo.co.jp>
2023-02-19 11:50:22 +0900

    seederとFactory準備

---
+++ src/database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'body' => Str::random(140),
+        ]);
+    }
+}
