5f2301ae9c7069e7bc03a9ece94bace5afa872eb
鳥井真菜子 <toriimana5@yahoo.co.jp>
2023-02-26 16:00:52 +0900

    投稿機能とバリデーションを一旦実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index'])->name('post');
+Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+Route::post('/posts', [PostController::class, 'store'])->name('post.store');
