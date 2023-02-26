d42764771279d24ec060874d4cfccaca0102d1a4
鳥井真菜子 <toriimana5@yahoo.co.jp>
2023-02-26 13:09:39 +0900

    ルーティング設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/home', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
