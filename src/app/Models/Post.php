b8809e5bd3d5c5ef653d471aae1a3a7d4a72f4ae
鳥井真菜子 <toriimana5@yahoo.co.jp>
2023-02-26 13:30:09 +0900

    コンフリクト解消

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -7,12 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-<<<<<<< HEAD
     use HasFactory;
-=======
     public function user()
     {
         return $this->belongsTo(User::class);
     }
->>>>>>> feature/#4
 }
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,12 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-<<<<<<< HEAD
     use HasFactory;
-=======
     public function posts()
     {
         return $this->hasMany(Post::class);
     }
->>>>>>> feature/#4
 }
