0963d1cba0c3b99e96ee0bec67a84e946617c49e
鳥井 真菜子 <toriimanako@yahoo.co.jp>
2023-06-23 14:55:04 +0900

    プロフィール登録と修正機能の修正

---
+++ src/app/Http/Requests/EditProfileRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'body.0' => ['max:140'],
+            'body.1' => ['required', 'between:4,50']
+        ];
+    }
+}
--- src/app/Http/Requests/RegisterProfileRequest.php
+++ src/app/Http/Requests/RegisterProfileRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProfileRequest extends FormRequest
+class registerProfileRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,7 +25,6 @@ class ProfileRequest extends FormRequest
     {
         return [
             'body.0' => ['max:140'],
-            'body.1' => ['required', 'between:4,50']
         ];
     }
 }
