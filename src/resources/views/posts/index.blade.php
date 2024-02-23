fd478ffaeba76ff3c5213df2eb80a27db86d78cf
鳥井真菜子 <toriimana5@yahoo.co.jp>
2024-02-23 16:47:07 +0900

    ログインしているユーザーと投稿者のユーザーIDが等しい場合に投稿内容を修正できるように認可を追加

--- src/resources/views/posts/index.blade.php
+++ src/resources/views/posts/index.blade.php
@@ -22,8 +22,10 @@
                 <p>ユーザー名: {{ link_to_route('profile.show', $title = $post->user->username, $parameters = [$post->user->username]) }}</p>
                 <p>ニックネーム: {{ $post->user->display_name }}</p>
                 <p>投稿内容: {{ $post->body }}</p>
-                <p>{{ link_to_route('post.edit', $title = "内容を編集", $parameters = [$post->id]) }}</p>
-                <br>
+				@can ('update-post', $post)
+                	<p>{{ link_to_route('post.edit', $title = "内容を編集", $parameters = [$post->id]) }}</p>
+				@endcan
+				<br>
                 @if (!is_null($post->image))
                     <img src="{{ asset(Storage::url($post->image)) }}">
                 @endif
