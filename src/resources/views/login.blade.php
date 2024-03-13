7f87f58bee0f689809fd71b8a1977ab5954d1040
鳥井真菜子 <toriimana5@yahoo.co.jp>
2024-03-13 14:53:41 +0900

    ログインページの修正

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -3,31 +3,38 @@
 @section('title', 'ログイン')
 
 @section('content')
+<div>
     {{ Form::open(['route' => 'user.authenticate']) }}
+    <div class="form-row">
         @error('error')
-            <div>{{ $error }}</div>
-        @enderror
-       メールアドレス: {{ Form::text('email', old('email')) }}
-       <br>
-       @if ($errors->has('email'))
-            <p>
-                {{ $errors->first('email') }}
-            </p>
-        @endif
-       パスワード: {{ Form::password('password') }}
-       <br>
-       @if ($errors->has('password'))
-            <p>
-                {{ $errors->first('password') }}
-            </p>
-        @endif
-
-        @if (session('flash_message'))
-			<div>
-				{{ session('flash_message') }}
-			</div>
-		@endif
-        {{ Form::button('ログイン', ['type' => 'submit']) }}
+                <div>{{ $error }}</div>
+            @enderror
+            <div class="form-group">
+                {{ Form::label('email', 'メールアドレス') }}
+                {{ Form::text('email', old('email'), ['class' => 'form-control']) }}
+            </div>
+            @if ($errors->has('email'))
+                <p>
+                    {{ $errors->first('email') }}
+                </p>
+            @endif
+            <div class="form-group">
+                {{ Form::label('password', 'パスワード') }}
+                {{ Form::password('password', ['class' => 'form-control']) }}
+            </div>
+            @if ($errors->has('password'))
+                <p>
+                    {{ $errors->first('password') }}
+                </p>
+            @endif
+            @if (session('flash_message'))
+                <div>
+                    {{ session('flash_message') }}
+                </div>
+            @endif
+            {{ Form::button('ログイン', ['type' => 'submit', 'class' => 'btn btn-primary']) }}
+    </div>
     {{ Form::close() }}
-        {{ link_to_route('password_reset.email.form', $title = "パスワードをお忘れの方") }}
+    {{ link_to_route('password_reset.email.form', $title = "パスワードをお忘れの方") }}
+</div>
 @endsection
