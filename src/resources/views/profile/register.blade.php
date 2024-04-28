f2c87b83ce5c1f6323ad42525c08e179ffa0ad9b
鳥井真菜子 <toriimana5@yahoo.co.jp>
2024-04-28 15:03:04 +0900

    フォーム部分の修正

--- src/resources/views/profile/register.blade.php
+++ src/resources/views/profile/register.blade.php
@@ -3,38 +3,39 @@
 @section('title', 'プロフィール登録')
 
 @section('content')
-<div id="margin_top">
+<div class="container" id="margin_top">
     {{ link_to_route('profile.show', $title = "プロフィールに戻る", $parameters = ['userName' => $userName]) }}
-    <br>
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-    {{ Form::open(['route' => ['profile.register.complete', $userName], 'files' => true]) }}
-    <div class="form-row">
-        <div class="col-sm-6">
-            <div class="form-floating">
-                {{ Form::textarea('text', old('text'), ['class' => 'form-control', 'id' => 'floatingInput', 'placeholder' => '']) }}
-                {{ Form::label('floatingInput', 'プロフィール', ['label' => 'floatingInput']) }}
+    <div class="container card">
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+        {{ Form::open(['route' => ['profile.register.complete', $userName], 'files' => true]) }}
+        <div class="form-row">
+            <div class="col-sm-6 mt-3">
+                <div class="form-floating">
+                    {{ Form::textarea('text', old('text'), ['class' => 'form-control', 'id' => 'floatingInput', 'placeholder' => '']) }}
+                    {{ Form::label('floatingInput', 'プロフィール', ['label' => 'floatingInput']) }}
+                </div>
             </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="form-floating">
-                {{ Form::label('profileIconImage', 'アイコン画像') }}
-                {{ Form::file('profileIconImage', ['class' => 'form-control']) }}
+            <div class="col-sm-6 mt-3">
+                <div class="input-group">
+                    {{ Form::label('profileIconImage', 'アイコン画像', ['class' => 'input-group-text']) }}
+                    {{ Form::file('profileIconImage', ['class' => 'form-control', 'id' => 'profileIconImage']) }}
+                </div>
             </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="form-floating">
-                {{ Form::label('profileBackground', '背景画像') }}
-                {{ Form::file('profileBackground', ['class' => 'form-control']) }}
+            <div class="col-sm-6 mt-3">
+                <div class="input-group">
+                    {{ Form::label('profileBackground', '背景画像', ['class' => 'input-group-text']) }}
+                    {{ Form::file('profileBackground', ['class' => 'form-control', 'id' => 'profileBackground']) }}
+                </div>
             </div>
+            {{ Form::button('編集', ['type' => 'submit', 'class' => 'btn btn-primary']) }}
         </div>
-        {{ Form::button('編集', ['type' => 'submit', 'class' => 'btn btn-primary']) }}
+        {{ Form::close() }}
     </div>
-    {{ Form::close() }}
 </div>
 @endsection
